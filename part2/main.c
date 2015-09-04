7437731662a0b5658a8b282163e9a3d9ffa23a4d
Divay Prakash <divay14039@iiitd.ac.in>
2015-09-04 13:26:39 +0530

    Few changes to part 2

--- part2/main.c
+++ part2/main.c
@@ -8,8 +8,7 @@
 
 
 //**********Function prototypes**********//
-void init_check_wdt(void);	//to perform initial check on WDT and reset if required
-void setup_wdt(void);		//to setup WDT for interrupt mode with interrupt interval 1s
+void init();
 //***************************************// 
 
 
@@ -22,9 +21,7 @@ volatile int flag = 0;
 int main(void) 
 {
     cli();									//disable interrupts
-    DDRB |= 0x20;							//set PORTB5 as output
-    init_check_wdt();						//initial check of WDT
-    setup_wdt();							//setup WDT
+    init();									//initialiazation function
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);    //set sleep mode  
     sei();                                  //enables interrupts
     while(1) {                              //main loop
@@ -40,27 +37,21 @@ int main(void)
 //*********************************//
 
 
-//**********Function to check WDT state at start**********//
-void init_check_wdt(void)
+//**********Initialize function**********// 
+void init()
 {
-    if(MCUSR & _BV(WDRF))
-    {                  //if prev reset caused by WDT
-        MCUSR &= ~_BV(WDRF);                //clear WDT reset flag
-        WDTCSR |= (_BV(WDCE) | _BV(WDE));   //enable WDCE
-        WDTCSR = 0x00;                      //disable WDT
+    if(MCUSR & _BV(WDRF))							//if prev reset caused by WDT
+    {                  
+        MCUSR &= ~_BV(WDRF);                		//clear WDT reset flag
+        WDTCSR |= (_BV(WDCE) | _BV(WDE));   		//enable WDCE
+        WDTCSR = 0x00;                      		//disable WDT
     }
-}
-//********************************************************//
-
-
-//**********Function to setup WDT**********// 
-void setup_wdt(void)
-{
+    DDRB |= 0x20;									//set PORTB5 as output
     WDTCSR |= (_BV(WDCE) | _BV(WDE));               //enable WDCE, set WDT to interrupt mode
     WDTCSR =  _BV(WDIE) |  _BV(WDP2) | _BV(WDP1);	//set WDT timeout to ~1 seconds
     WDTCSR |= _BV(WDIE);
 }
-//*****************************************//
+//***************************************//
 
    
 //**********WDT Interrupt Service Routine**********//   
